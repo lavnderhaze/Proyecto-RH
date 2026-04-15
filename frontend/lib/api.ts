6d7902c151737d8f714cc3f5e123a201f05a6953
Daniela <danielacb2013@gmail.com>
2026-04-14 21:35:54 -0500

    push prueba pipeline

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -5,7 +5,7 @@ import type {
 } from "./types"
 
 const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "https://localhost:7076"
+  process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:54177"
 
 async function fetchApi<T>(
   endpoint: string,
